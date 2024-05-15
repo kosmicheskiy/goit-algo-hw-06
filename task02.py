2ea6b9680c8c84ca3b0b69763c2e0989ad3048e7
kosmicheskiy <bezvoshchuk.v@gmail.com>
2024-05-15 17:31:05 +0300

    Update task02.py

--- task02.py
+++ task02.py
@@ -1,23 +1,27 @@
 #BFS
-def find_all_paths_using_breadth_first_search(graph, start, end, path=[]):
-    path = path + [start]
-    if start == end:
-        return [path]
-    if start not in graph:
-        return []
+def find_all_paths_using_breadth_first_search(graph, start, end):
+    queue = [(start, [start])]
     paths = []
-    for node in graph[start]:
-        if node not in path:
-            new_paths = find_all_paths_using_breadth_first_search(graph, node, end, path)
-            for new_path in new_paths:
-                paths.append(new_path)
+    while queue:
+        current, path = queue.pop(0)
+        for neighbor in graph.neighbors(current):
+            if neighbor not in path:
+                new_path = path + [neighbor]
+                queue.append((neighbor, new_path))
+                if neighbor == end:
+                    paths.append(new_path)
     return paths
 
 # Find all path between nodes "Alice" and "Bob"
-all_paths = find_all_paths_using_breadth_first_search(dict(G.adjacency()), "Alice", "Charlie")
+all_paths = find_all_paths_using_breadth_first_search(G, "Alice", "Charlie")
 print("All paths between nodes Alice and Charlie using BFS:")
 for path in all_paths:
     print(path)
+
+#All paths between nodes Alice and Charlie using BFS:
+#['Alice', 'Charlie']
+#['Alice', 'David', 'Charlie']
+#['Alice', 'Bob', 'David', 'Charlie']
 
 
 #DFS
@@ -25,18 +29,21 @@ def find_all_paths__using_depth_first_search(graph, start, end, path=[]):
     path = path + [start]
     if start == end:
         return [path]
-    if start not in graph:
-        return []
     paths = []
-    for node in graph[start]:
-        if node not in path:
-            new_paths = find_all_paths__using_depth_first_search(graph, node, end, path)
-            for new_path in new_paths:
-                paths.append(new_path)
+    for neighbor in graph.neighbors(start):
+          if neighbor not in path:
+              new_paths = find_all_paths__using_depth_first_search(graph, neighbor, end, path)
+              for p in new_paths:
+                  paths.append(p)
     return paths
 
 # Find all path between nodes "Alice" and "Bob"
-all_paths_dfs = find_all_paths__using_depth_first_search(dict(G.adjacency()), "Alice", "Charlie")
+all_paths_dfs = find_all_paths__using_depth_first_search(G, "Alice", "Charlie")
 print("All paths between nodes Alice and Charlie using DFS:")
 for path in all_paths_dfs:
     print(path)
+
+#All paths between nodes Alice and Charlie using DFS:
+#['Alice', 'Bob', 'David', 'Charlie']
+#['Alice', 'Charlie']
+#['Alice', 'David', 'Charlie']
