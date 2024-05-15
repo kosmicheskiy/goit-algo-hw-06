9c296749799fa62bf77b1821f954af0172317a6d
kosmicheskiy <bezvoshchuk.v@gmail.com>
2024-05-15 17:52:42 +0300

    Update task03.py

--- task03.py
+++ task03.py
@@ -1,8 +1,31 @@
 # Find shortest path between all nodes
-shortest_paths = dict(nx.all_pairs_dijkstra_path(G))
+def dijkstra_algorithm(graph, start):
+    distances = {vertex: float("infinity") for vertex in graph}
+    distances[start] = 0
+    not_visited = list(graph.nodes)
 
-for source in shortest_paths:
-    for target in shortest_paths[source]:
-        path = shortest_paths[source][target]
-        length = nx.dijkstra_path_length(G, source, target)
-        print(f"Shortes path between nodes {source} and {target}: {path}, length = {length}")
+    while not_visited:
+        current_vertex = min(not_visited, key=lambda vertex: distances[vertex])
+
+        if distances[current_vertex] == float("infinity"):
+            break
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = distances[current_vertex] + weight["weight"]
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+
+        not_visited.remove(current_vertex)
+
+    return distances
+
+for start in G.nodes:
+    shortest_paths = dijkstra_algorithm(G, start)
+    print(f"Shortes path from nodes {start}: {shortest_paths}")  
+
+#Shortes path from nodes Alice: {'Alice': 0, 'Bob': 4, 'Charlie': 2, 'David': 5, 'Eve': 5}
+#Shortes path from nodes Bob: {'Alice': inf, 'Bob': 0, 'Charlie': 17, 'David': 10, 'Eve': 16}
+#Shortes path from nodes Charlie: {'Alice': inf, 'Bob': inf, 'Charlie': 0, 'David': inf, 'Eve': 3}
+#Shortes path from nodes David: {'Alice': inf, 'Bob': inf, 'Charlie': 7, 'David': 0, 'Eve': 6}
+#Shortes path from nodes Eve: {'Alice': inf, 'Bob': inf, 'Charlie': inf, 'David': inf, 'Eve': 0}
